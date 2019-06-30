9a3d59df3f8e12bf4ed9ee330667b0897cd21759
atilol <ggrkssk@gmail.com>
2019-06-30 16:48:50 +0900

    abc132

---
+++ abc/132/d/combination/main.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+var (
+	readString func() string
+	readBytes  func() []byte
+)
+
+func init() {
+	readString, readBytes = newReadString(os.Stdin)
+}
+
+func newReadString(ior io.Reader) (func() string, func() []byte) {
+	r := bufio.NewScanner(ior)
+	r.Buffer(make([]byte, 1024), int(1e+11))
+	r.Split(bufio.ScanWords)
+
+	f1 := func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+	f2 := func() []byte {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Bytes()
+	}
+	return f1, f2
+}
+
+func readInt() int {
+	return int(readInt64())
+}
+
+func readInt64() int64 {
+	i, err := strconv.ParseInt(readString(), 10, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+func readFloat64() float64 {
+	f, err := strconv.ParseFloat(readString(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func minInt64(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func maxInt64(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func absInt64(a int64) int64 {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func sum(a []int) int {
+	var ret int
+	for i := range a {
+		ret += a[i]
+	}
+	return ret
+}
+
+func sumInt64(a []int64) int64 {
+	var ret int64
+	for i := range a {
+		ret += a[i]
+	}
+	return ret
+}
+
+func sumFloat64(a []float64) float64 {
+	var ret float64
+	for i := range a {
+		ret += a[i]
+	}
+	return ret
+}
+
+func gcdInt64(m, n int64) int64 {
+	for m%n != 0 {
+		m, n = n, m%n
+	}
+	return n
+}
+
+func lcmInt64(m, n int64) int64 {
+	return m / gcdInt64(m, n) * n
+}
+
+// sort ------------------------------------------------------------
+
+type int64Array []int64
+
+func (s int64Array) Len() int           { return len(s) }
+func (s int64Array) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s int64Array) Less(i, j int) bool { return s[i] < s[j] }
+
+type xxx struct {
+	x int
+}
+
+type sortArray []xxx
+
+func (s sortArray) Len() int           { return len(s) }
+func (s sortArray) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s sortArray) Less(i, j int) bool { return s[i].x < s[j].x }
+
+// -----------------------------------------------------------------
+
+/*
+法 1e9+7(1000000007) の下での2項係数(combination)
+*/
+var n int64 = 2000 + 1
+var mod int64 = 1000000007
+var fac = make([]int64, n)
+var inv = make([]int64, n)
+var finv = make([]int64, n)
+
+func initComb() {
+	fac[0], fac[1] = 1, 1
+	finv[0], finv[1] = 1, 1
+	inv[1] = 1
+
+	var i int64
+	for i = 2; i < n; i++ {
+		fac[i] = fac[i-1] * i % mod
+		inv[i] = mod - inv[mod%i]*(mod/i)%mod
+		finv[i] = finv[i-1] * inv[i] % mod
+	}
+}
+
+func comb(n, k int) int64 {
+	if n < k || n < 0 || k < 0 {
+		return 0
+	}
+	return fac[n] * (finv[k] * finv[n-k] % mod) % mod
+}
+
+func main() {
+	n := readInt()
+	k := readInt()
+
+	initComb()
+	for i := 1; i <= k; i++ {
+		ans := comb(n-k+1, i) * comb(k-1, i-1)
+		fmt.Println(ans % mod)
+	}
+}
