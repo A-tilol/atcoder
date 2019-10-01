038b97683d08ac4005ceb6b5b4458a6d09aca9e3
atilol <ggrkssk@gmail.com>
2019-10-01 13:29:22 +0900

    new

---
+++ abc/137/c/main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+)
+
+/*
+Combination 組み合わせ
+*/
+func comb(n, r int) int {
+	if r == 0 {
+		return 1
+	}
+	return comb(n, r-1) * (n - r + 1) / r
+}
+
+func main() {
+	n := readInt()
+	m := make(map[string]int)
+	for i := 0; i < n; i++ {
+		s := sortArray(readBytes())
+		sort.Sort(s)
+		m[string(s)]++
+	}
+
+	ans := 0
+	for _, v := range m {
+		if v < 2 {
+			continue
+		}
+		ans += comb(v, 2)
+	}
+	fmt.Println(ans)
+}
+
+// sort ------------------------------------------------------------
+
+type xxx struct {
+	x int
+}
+
+type sortArray []byte
+
+func (s sortArray) Len() int           { return len(s) }
+func (s sortArray) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s sortArray) Less(i, j int) bool { return s[i] < s[j] }
+
+// -----------------------------------------------------------------
+
+var (
+	readString func() string
+	readBytes  func() []byte
+)
+
+func init() {
+	readString, readBytes = newReadString(os.Stdin)
+}
+
+func newReadString(ior io.Reader) (func() string, func() []byte) {
+	r := bufio.NewScanner(ior)
+	r.Buffer(make([]byte, 1024), int(1e+11))
+	r.Split(bufio.ScanWords)
+
+	f1 := func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+	f2 := func() []byte {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Bytes()
+	}
+	return f1, f2
+}
+
+func readInt() int {
+	return int(readInt64())
+}
+
+func readInt64() int64 {
+	i, err := strconv.ParseInt(readString(), 10, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+func readFloat64() float64 {
+	f, err := strconv.ParseFloat(readString(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func sum(a []int) int {
+	var ret int
+	for i := range a {
+		ret += a[i]
+	}
+	return ret
+}
+
+func sumFloat64(a []float64) float64 {
+	var ret float64
+	for i := range a {
+		ret += a[i]
+	}
+	return ret
+}
+
+func gcd(m, n int) int {
+	for m%n != 0 {
+		m, n = n, m%n
+	}
+	return n
+}
+
+func lcm(m, n int) int {
+	return m / gcd(m, n) * n
+}
