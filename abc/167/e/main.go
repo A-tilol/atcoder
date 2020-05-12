800aefbc82c45cc8b1754b59ec9d8c26fe3993c2
atilol <ggrkssk@gmail.com>
2020-05-12 09:09:36 +0900

    update

---
+++ abc/167/e/main.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+var nmax = int(2*1e5 + 1)
+var mod = 998244353
+var fac = make([]int, nmax)
+var inv = make([]int, nmax)
+var finv = make([]int, nmax)
+
+func initComb() {
+	fac[0], fac[1] = 1, 1
+	finv[0], finv[1] = 1, 1
+	inv[1] = 1
+
+	for i := 2; i < nmax; i++ {
+		fac[i] = fac[i-1] * i % mod
+		inv[i] = mod - inv[mod%i]*(mod/i)%mod
+		finv[i] = finv[i-1] * inv[i] % mod
+	}
+}
+
+func powMod(x, i int) int {
+	if i == 0 {
+		return 1
+	}
+
+	y := 1
+	if i%2 != 0 {
+		y = x
+		i--
+	}
+
+	t := powMod(x, i/2)
+	return y * t % mod * t % mod
+}
+
+func main() {
+	n, m, k := readInt(), readInt(), readInt()
+
+	initComb()
+
+	ans := 0
+	for i := 0; i <= k; i++ {
+		ans += m * powMod(m-1, n-i-1) % mod * fac[n-1] % mod * finv[n-i-1] % mod * finv[i] % mod
+		ans %= mod
+	}
+
+	fmt.Println(ans)
+}
+
+// sort ------------------------------------------------------------
+
+type xxx struct {
+	x int
+}
+
+type sortArray []xxx
+
+func (s sortArray) Len() int           { return len(s) }
+func (s sortArray) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s sortArray) Less(i, j int) bool { return s[i].x < s[j].x }
+
+// -----------------------------------------------------------------
+
+var (
+	readString func() string
+	readBytes  func() []byte
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+	readString, readBytes = newReadString(os.Stdin)
+}
+
+func newReadString(ior io.Reader) (func() string, func() []byte) {
+	r := bufio.NewScanner(ior)
+	r.Buffer(make([]byte, 1024), int(1e+11))
+	r.Split(bufio.ScanWords)
+
+	f1 := func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+	f2 := func() []byte {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Bytes()
+	}
+	return f1, f2
+}
+
+func readInt() int {
+	return int(readInt64())
+}
+
+func readInt64() int64 {
+	i, err := strconv.ParseInt(readString(), 10, 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+func readFloat64() float64 {
+	f, err := strconv.ParseFloat(readString(), 64)
+	if err != nil {
+		panic(err.Error())
+	}
+	return f
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func sum(a []int) int {
+	var ret int
+	for i := range a {
+		ret += a[i]
+	}
+	return ret
+}
+
+func sumFloat64(a []float64) float64 {
+	var ret float64
+	for i := range a {
+		ret += a[i]
+	}
+	return ret
+}
+
+func gcd(m, n int) int {
+	for m%n != 0 {
+		m, n = n, m%n
+	}
+	return n
+}
+
+func lcm(m, n int) int {
+	return m / gcd(m, n) * n
+}
