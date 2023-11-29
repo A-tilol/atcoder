d13bff3a83874784cdffa281efea433f28ae6a48
atilol <ggrkssk@gmail.com>
2023-11-29 14:37:21 +0900

    -

--- abc/327/c/main.py
+++ abc/327/c/main.py
@@ -39,4 +39,31 @@ def get_logger(debug=True) -> logging.Logger:
 
 
 # -------------------------------
-log = get_logger(True)
+log = get_logger(False)
+
+a = [None] * 9
+for i in range(9):
+    a[i] = read_ints()
+
+for i in range(9):
+    if len(set(a[i])) != 9:
+        print("No")
+        exit()
+
+for i in range(9):
+    s = set()
+    for j in range(9):
+        s.add(a[j][i])
+    if len(s) != 9:
+        print("No")
+        exit()
+
+for i in range(0, 9, 3):
+    for j in range(0, 9, 3):
+        s = set()
+        for k in range(3):
+            s.update(a[i + k][j : j + 3])
+        if len(s) != 9:
+            print("No")
+            exit()
+print("Yes")
