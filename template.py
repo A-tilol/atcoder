61c2e8c5da910e82dac54e9061e044ff64611615
igarashi <igarashi.shunta@canon-elec.co.jp>
2024-06-17 10:50:34 +0900

    -

--- template.py
+++ template.py
@@ -21,26 +21,6 @@ def get_logger(debug=True):
     return logger
 
 
-def modpow(a: int, n: int, mod: int) -> int:
-    """二分累乗法"""
-    res = 1
-    while n > 0:
-        if n & 1:
-            res = res * a % mod
-        a = a * a % mod
-        n >>= 1
-    return res
-
-
-def modinv(a: int, mod: int) -> int:
-    """逆元の計算
-
-    オイラーの小定理より
-    法modにおけるaの逆元a^-1 = modpow(a, mod-2, mod)
-    """
-    return modpow(a, mod - 2, mod)
-
-
 # -------------------------------
 log = get_logger()
 
