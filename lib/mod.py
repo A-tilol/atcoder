d76d0b3c4327b3cd677e9837a6d4c4602579d615
atilol <ggrkssk@gmail.com>
2024-06-20 21:36:06 +0900

    -

--- lib/mod.py
+++ lib/mod.py
@@ -1,5 +1,6 @@
 def modpow(a: int, n: int, mod: int) -> int:
     """二分累乗法"""
+
     res = 1
     while n > 0:
         if n & 1:
@@ -13,6 +14,14 @@ def modinv(a: int, mod: int) -> int:
     """逆元の計算
 
     オイラーの小定理より
-    法modにおけるaの逆元a^-1 = modpow(a, mod-2, mod)
+    法modにおけるaの逆元a^-1 = a^(mod-2) % mod
     """
+
     return modpow(a, mod - 2, mod)
+
+
+def factorial(n: int, mod: int) -> int:
+    ret = 1
+    for i in range(2, n + 1):
+        ret = ret * i % mod
+    return ret
