033096f008ded53c021dd032afe1283b70057421
atilol <ggrkssk@gmail.com>
2024-06-23 17:46:43 +0900

    -

--- lib/mod.py
+++ lib/mod.py
@@ -20,8 +20,18 @@ def modinv(a: int, mod: int) -> int:
     return modpow(a, mod - 2, mod)
 
 
-def factorial(n: int, mod: int) -> int:
+def fact(n: int, mod: int) -> int:
     ret = 1
     for i in range(2, n + 1):
         ret = ret * i % mod
     return ret
+
+
+def ifact(n: int, mod: int) -> int:
+    return modinv(fact(n, mod), mod)
+
+
+def modcomb(n: int, r: int, mod: int) -> int:
+    if n < 0 or r < 0 or n < r:
+        return 0
+    return fact(n, mod) * ifact(n - r, mod) % mod * ifact(r, mod) % mod
