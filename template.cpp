8d062efcbb4025721e43973ceddbf0eb3608fb86
atilol <ggrkssk@gmail.com>
2024-07-24 08:09:39 +0900

    wip: add cpp template

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define REP0(n) for (auto _i = std::decay_t<decltype(n)>{}; _i != (n); ++_i)
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; i != (n); ++i)
+#define REP2(i, l, r) for (auto i = (l); i != (r); ++i)
+#define OVERLOAD_REP(_1, _2, _3, NAME, ...) NAME
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1, REP0)(__VA_ARGS__)
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
